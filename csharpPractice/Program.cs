9e112ce4563e2baf109b3ebc08c3f9c3935f47e8
azizulhakim13 <mah.azizulhakim.sust@gmail.com>
2024-02-14 12:20:53 +0600

    Add project files.

---
+++ csharpPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MyCsharpProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Not have new line");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("nothing");
+
+            // Escape Sequence
+            /*
+             * \b \t
+            */
+
+            // ====>>>>> Variables & data types
+            int x;   // Declaration
+            x = 1;
+            double y = 2.5;
+            double result = x + y;
+            bool alive = true;
+
+            Console.WriteLine("The result is: " + result);
+        }
+    }
+}
