2a01a97e0401da5ca817042179cb184d97b54ec9
azizulhakim13 <mah.azizulhakim.sust@gmail.com>
2024-02-20 14:53:06 +0600

    conditional statements and loops

--- csharpPractice/Program.cs
+++ csharpPractice/Program.cs
@@ -81,6 +81,74 @@
             Console.WriteLine(userName + ", Length: " + userName.Length);
             Console.WriteLine(userName.Substring(0, 5));    // slice of code
 
+
+            //======>>>>>>>> If Statement, Logical Operator(&&, ||)
+            Console.WriteLine("Please Enter Your Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age > 18 || age == 18)
+            {
+                Console.WriteLine("You are now signed up!");
+            }
+            else if (age < 0)
+            {
+                Console.WriteLine("Born first!");
+            }
+            else
+            {
+                Console.WriteLine("You Must be 18+ to signup!");
+            }
+
+            Console.WriteLine("Enter your name: ");
+            string nameUser = Console.ReadLine();
+            if (nameUser == "")
+            {
+                Console.WriteLine("You haven't entered your name");
+            }
+            else
+            {
+                Console.WriteLine("Hello " + nameUser);
+            }
+
+            // Switch case
+            Console.WriteLine("What day is it today?");
+            string day = Console.ReadLine();
+            switch (day)
+            {
+                case "Monday":
+                    Console.WriteLine("It's Monday!");
+                    break;
+                case "Tuesday":
+                    Console.WriteLine("It's Tuesday!");
+                    break;
+                default:
+                    Console.WriteLine(day + " is not day!");
+                    break;
+            }
+
+
+            //=======>>>>>>> Loop: While loop => repeat code based on a condition trueConsole.Write("Enter your name: ");
+            string yourName = "";
+            while (yourName == "")
+            {
+                Console.Write("Enter your name: ");
+                yourName = Console.ReadLine();
+            }
+            Console.WriteLine("Hello " + yourName);
+
+            // For loop
+            for (int index = 1; index <= 10; index += 2)
+            {
+                Console.Write(index + "/");
+            }
+
+            for (int yy = 10; yy > 0; yy -= 2)
+            {
+                Console.Write(yy);
+            }
+
+            // nested loop
+
+
         }
     }
 }
