badfe0ba681cc03474e602c11f95ea1bb016950f
azizulhakim13 <mah.azizulhakim.sust@gmail.com>
2024-02-22 13:05:36 +0600

    loop, method, some problem solve

--- csharpPractice/Program.cs
+++ csharpPractice/Program.cs
@@ -2,8 +2,20 @@
 {
     class Program
     {
+        // =======>>>>> Method
+        static void Message(string name, int age)
+        {
+            Console.WriteLine("Hello: " + name + ", you're: " + age);
+        }
+
         static void Main(string[] args)
         {
+            // =====>>>> Method Calling
+            string yourName = "John";
+            int yourAge = 30;
+            Message(yourName, yourAge);
+
+
             Console.Write("Not have new line");
             Console.WriteLine("Hello World");
             Console.WriteLine("nothing");
@@ -82,73 +94,156 @@
             Console.WriteLine(userName.Substring(0, 5));    // slice of code
 
 
-            //======>>>>>>>> If Statement, Logical Operator(&&, ||)
-            Console.WriteLine("Please Enter Your Age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            if (age > 18 || age == 18)
-            {
-                Console.WriteLine("You are now signed up!");
-            }
-            else if (age < 0)
-            {
-                Console.WriteLine("Born first!");
-            }
-            else
-            {
-                Console.WriteLine("You Must be 18+ to signup!");
-            }
+            ////======>>>>>>>> If Statement, Logical Operator(&&, ||)
+            //Console.WriteLine("Please Enter Your Age: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //if (age > 18 || age == 18)
+            //{
+            //    Console.WriteLine("You are now signed up!");
+            //}
+            //else if (age < 0)
+            //{
+            //    Console.WriteLine("Born first!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You Must be 18+ to signup!");
+            //}
 
-            Console.WriteLine("Enter your name: ");
-            string nameUser = Console.ReadLine();
-            if (nameUser == "")
-            {
-                Console.WriteLine("You haven't entered your name");
-            }
-            else
-            {
-                Console.WriteLine("Hello " + nameUser);
-            }
+            //Console.WriteLine("Enter your name: ");
+            //string nameUser = Console.ReadLine();
+            //if (nameUser == "")
+            //{
+            //    Console.WriteLine("You haven't entered your name");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hello " + nameUser);
+            //}
 
-            // Switch case
-            Console.WriteLine("What day is it today?");
-            string day = Console.ReadLine();
-            switch (day)
-            {
-                case "Monday":
-                    Console.WriteLine("It's Monday!");
-                    break;
-                case "Tuesday":
-                    Console.WriteLine("It's Tuesday!");
-                    break;
-                default:
-                    Console.WriteLine(day + " is not day!");
-                    break;
-            }
+            //// Switch case
+            //Console.WriteLine("What day is it today?");
+            //string day = Console.ReadLine();
+            //switch (day)
+            //{
+            //    case "Monday":
+            //        Console.WriteLine("It's Monday!");
+            //        break;
+            //    case "Tuesday":
+            //        Console.WriteLine("It's Tuesday!");
+            //        break;
+            //    default:
+            //        Console.WriteLine(day + " is not day!");
+            //        break;
+            //}
 
 
-            //=======>>>>>>> Loop: While loop => repeat code based on a condition trueConsole.Write("Enter your name: ");
-            string yourName = "";
-            while (yourName == "")
-            {
-                Console.Write("Enter your name: ");
-                yourName = Console.ReadLine();
-            }
-            Console.WriteLine("Hello " + yourName);
+            ////=======>>>>>>> Loop: While loop => repeat code based on a condition trueConsole.Write("Enter your name: ");
+            //string yourName = "";
+            //while (yourName == "")
+            //{
+            //    Console.Write("Enter your name: ");
+            //    yourName = Console.ReadLine();
+            //}
+            //Console.WriteLine("Hello " + yourName);
 
-            // For loop
-            for (int index = 1; index <= 10; index += 2)
-            {
-                Console.Write(index + "/");
-            }
+            //// For loop
+            //for (int index = 1; index <= 10; index += 2)
+            //{
+            //    Console.Write(index + "/");
+            //}
 
-            for (int yy = 10; yy > 0; yy -= 2)
-            {
-                Console.Write(yy);
-            }
+            //for (int yy = 10; yy > 0; yy -= 2)
+            //{
+            //    Console.Write(yy);
+            //}
 
             // nested loop
+            //Console.Write("How many Rows: ");
+            //int rows = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("How many columns: ");
+            //int columns = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("What is the Symbol: ");
+            //string symbol = Console.ReadLine();
+
+            //for (int a = 0; a < rows; a++)
+            //{
+            //    for (int ab = 0; ab < columns; ab++)
+            //    {
+            //        Console.Write(symbol);
+            //    }
+            //    Console.WriteLine();
+            //}
 
 
+            // ====>>>> Array
+            string[] cars = { "Toyota", "Volvo", "Hyundai" };
+            for (int count = 0; count < cars.Length; count++)
+            {
+                Console.Write(cars[count] + ", ");
+            }
+
+            // Iterating array with foreach
+            foreach (string car in cars)
+            {
+                Console.Write(car + ", ");
+            }
+
+            // Fixed sized array
+            string[] fruits = new string[2];
+            fruits[0] = "Mango";
+
+            //Sum of all elements in an array
+            int[] numbers = { 2, 4, 7, 8, 9, 10, 11 };
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum = sum + number;
+            }
+            Console.WriteLine(sum);
+
+            //find the maxiumum number in an array
+            int[] numbers2 = { 4, 7, 8, 9, 3, 1 };
+            int max = numbers2[0];
+            for (int count = 1; count < numbers2.Length; count++)
+            {
+                if (numbers2[count] > max)
+                {
+                    max = numbers2[count];
+                }
+            }
+            Console.WriteLine(max);
+
+            // find Count of a number
+            Console.Write("Write a number to find it's Count: ");
+            int findCount = Convert.ToInt32(Console.ReadLine());
+            for (int count = 1; count <= 10; count++)
+            {
+                result = findCount * count;
+                Console.WriteLine($"{findCount} × {count} = " + result);
+            }
+
+
+            // ===>>> Method
+            double num1;
+            double num2;
+            double resultMultiplication;
+            Console.WriteLine("Enter number 1: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter number 1: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            resultMultiplication = Multiply(num1, num2);
+            Console.WriteLine(resultMultiplication);
         }
+
+        static double Multiply(double a, double b)
+        {
+            double z = a * b;
+            return z;
+        }
+
+
     }
 }
